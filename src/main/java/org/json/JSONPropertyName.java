22d8e09eab114fdaefad940a4f4865bb66b54a8b
Yung Sheng Cheng <yungsheng.cheng@mail.utoronto.ca>
2020-06-19 23:51:20 -0400

    Imported JSON library.

---
+++ src/main/java/org/json/JSONPropertyName.java
@@ -0,0 +1,19 @@
+/*
+ * Decompiled with CFR 0.145.
+ */
+package org.json;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(value=RetentionPolicy.RUNTIME)
+@Target(value={ElementType.METHOD})
+public @interface JSONPropertyName {
+    public String value();
+}
+
---
+++ src/main/java/org/json/JSONString.java
@@ -0,0 +1,9 @@
+/*
+ * Decompiled with CFR 0.145.
+ */
+package org.json;
+
+public interface JSONString {
+    public String toJSONString();
+}
+
